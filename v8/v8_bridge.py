10ef870f678fdb52b3b8fe364019888b47fd4bd5
Yusung Sim <yusungsim@gmail.com>
2020-06-11 16:31:07 +0900

    class-wrapping v8 bridge

--- v8/v8_bridge.py
+++ v8/v8_bridge.py
@@ -1,8 +1,5 @@
 from py_mini_racer import py_mini_racer as pmr
 
-def get_js_context():
-  return pmr.MiniRacer()
-
 # Supporting console functions in test codes
 # need to override console functions, such as console.log, console.err, etc
 # and when execution is complete, collect the results
@@ -10,18 +7,30 @@ def override_console(ctx):
   ovrr_file = open("override_console.js", 'r')
   ctx.eval(ovrr_file.read())
 
-def collect_console(ctx):
-  pass
+def snapshot_console(ctx):
+  console_stack = ctx.eval("log_stack")
+  return console_stack
+
+class V8bridge():
+  # Setup a JS evaluation context
+  def __init__(self):
+    self.ctx = pmr.MiniRacer()
+    override_console(self.ctx)
+  
+  # eval a source code string
+  def eval_source(self, source):
+    self.ctx.eval(source)
+
+  # eval a js file
+  def eval_file(self, filename):
+    with open(filename, 'r') as js_file:
+      self.ctx.eval(js_file.read())
+  
+  # console output of js program is now stored at context's "log_stack"
+  def result_console(self):
+    return snapshot_console(self.ctx)
 
 def test_js_file(filename):
-  # first make js context for testing
-  ctx = get_js_context()
-  override_console(ctx)
-  # open target js code, and eval it in context
-  js_file = open(filename, 'r')
-  ctx.eval(js_file.read())
-  # finally print the resulting context's result
-  console_result = ctx.eval("log_stack")
-  print(console_result)
+  pass
 
 test_js_file("js_test.js")
