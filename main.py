5c5a41baa7a5245bd2154dd3b64846f17cf55232
okas832 <okas832@kaist.ac.kr>
2020-06-23 22:08:18 +0900

    Run diffing te test262

--- main.py
+++ main.py
@@ -2,28 +2,73 @@ from mutator.js_mutator import code_mutator
 from spidermonkey import Spidermonkey
 from v8.v8_bridge import V8bridge
 from differ.diff import diff
+import os
+import signal
 
-if __name__ == "__main__":
-    target_program = "./input/sample01.js"
-    with open(target_program, "r") as f:
-        Mutator = code_mutator(f.read()+"\nf();\n")
-        code = Mutator.gen_code()
-        m_code = Mutator.gen_mutant()
+def handler():
+    raise Exception("Timeout")
 
-        sm = Spidermonkey(early_script_file='-', code=m_code)
-        sm_out, sm_err = sm.communicate()
-
-        v8 = V8bridge()
-        try:
-            v8.eval_source(m_code)
-        except Exception as x:
-            v8_out = ""
-            v8_err = x.args[0].encode()
+def get_seed(d):
+    for f in os.listdir(d):
+        if os.path.isdir(d + f):
+            yield from get_seed(d + f + "/")
         else:
-            v8_out = v8.result_console()
-            v8_out = ("\n".join(v8_out) + "\n").encode()
-            v8_err = "".encode()
+            if f.endswith(".js"):
+                yield d + f
 
-        report = diff(sm_out, sm_err, v8_out, v8_err)
-        if report is not None:
-            print(report)
+save_dir = "report/"
+target_dir = "./test262/test/annexB/"
+
+signal.signal(signal.SIGALRM, handler)
+#for target_program in get_seed(target_dir):
+for target_program in ["./input/sample01.js"]:
+    print(target_program)
+    with open(target_program, "r") as f:
+        Mutator = code_mutator(f.read())
+        for i in range(2):
+            try:
+                m_code = Mutator.gen_mutant()
+            except:
+                break
+            print(m_code)
+            sm = Spidermonkey(early_script_file='-', code=m_code)
+            signal.alarm(2)
+            try:
+                sm_out, sm_err = sm.communicate()
+            except Exception as x:
+                sm_out = b""
+                sm_err = b"Timeout"
+            signal.alarm(0)
+
+            signal.alarm(2)
+            v8 = V8bridge()
+            try:
+                v8.eval_source(m_code)
+            except Exception as x:
+                v8_out = ""
+                if x.args[0] == "bTimeout":
+                    v8_err = b"Timeour"
+                elif isinstance(x.args[0], bytes):
+                    v8_err = x.args[0]
+                else:
+                    v8_err = x.args[0].encode()
+            else:
+                v8_out = v8.result_console()
+                v8_out = ("\n".join(v8_out) + "\n").encode()
+                v8_err = "".encode()
+            signal.alarm(0)
+            report = diff(sm_out, sm_err, v8_out, v8_err)
+
+            if report is not None:
+                with open(save_dir + target_program.split("/")[-1] + ".log", "wb") as p:
+                    p.write(m_code.encode())
+                    p.write(b"----------------------------\n")
+                    p.write(report[0])
+                    p.write(b"\n")
+                    p.write(report[1])
+                    p.write(b"\n")
+                    p.write(report[2])
+                    p.write(b"\n")
+                    p.write(report[3])
+                    p.write(b"\n")
+
